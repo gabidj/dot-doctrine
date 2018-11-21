9fdf3c067d36dca23d41f9bde932150e39b9e3a0
gabidj <git@gabisuciu.ro>
2018-11-21 18:41:32 +0200

    initial commit

---
+++ src/ConfigProvider.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by: DotKernel Team
+ * User: gabidjdev
+ * Date: 21/11/2018
+ * Time: 18:38
+ */
+
+namespace Dot\Doctrine;
+
+class ConfigProvider
+{
+    public function __invoke()
+    {
+        return [];
+    }
+}
